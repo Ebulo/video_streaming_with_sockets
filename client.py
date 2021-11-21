4c9ffbe27d21b0bab7a6f92472ef4d0f754f309b
Bishant Nayak <48652521+Ebulo@users.noreply.github.com>
2021-11-21 22:56:20 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,14 @@
-import cv2, socket, struct
+import cv2, socket, struct, random
 
 ip = socket.gethostbyname(socket.gethostname())
-port = 1001
+port = 1002
 
 soc = socket.socket()
 
 soc.connect((ip, port))
+
+def createMsg():
+  print("Creating A Message")
+  list_of_words =["Hey buddy","","Greetings!","Hey there!","What a surprise! Welcome bud","Hey there! Nice to meet you","How are you doing!","Hey"]
+  msg = list_of_words[random.randInt(0, len(list_of_words)-1)]
+  return msg
